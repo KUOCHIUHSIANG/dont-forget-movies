6a21eb40243190d33024463fe8d795790a39c294
Jater <p833795p@gmail.com>
2022-07-01 15:52:25 +0800

    style: 首頁icon 調整與背景、字體顏色設定

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,47 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #572d88;">
-    <a class="navbar-brand" href="#">莫忘</a>
+    <a class="navbar-brand" href="#">Don’t Forget !</a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <a class="nav-link" href="#">To Watch</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="請輸入電影名稱" aria-label="Search">
+      <input class="search-input mr-sm-2" type="search" placeholder="請輸入電影名稱" aria-label="Search">
       <div class="search-icon-wrapper">
         <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
       </div>
     </form>
   </nav>
 </template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Splash&display=swap');
+
+.navbar-brand {
+  user-select: none;
+  font-family: 'Splash', cursive;
+}
+
+
+// Small devices , > 576px 
+@media screen and (min-width: 576px) {
+  .navbar-brand {
+    font-size: 1.4em;
+  }
+}
+
+// Medium devices , > 768px
+@media screen and (min-width: 768px) {
+  .navbar-brand {
+    font-size: 1.8em;
+  }
+}
+
+// Large devices , > 992px
+// @media screen and (min-width: 992px) { ... }
+
+// Extra large devices , > 1200px
+// @media screen and (min-width: 1200px) { ... }
+
+</style>

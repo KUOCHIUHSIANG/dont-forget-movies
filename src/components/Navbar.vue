45b7414743e1cf3c85923d4c5c1f41cf50c76caa
Jater <p833795p@gmail.com>
2022-08-31 22:57:40 +0800

    style: 增加最小寬度，

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,57 +1,67 @@
 <template>
-<div class="nav-container">
-   <nav class="navbar navbar-expand-lg navbar-dark">
-    <router-link class="navbar-brand" to="/">Don’t Forget !</router-link>
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <router-link class="nav-link" to="/to-watch">To Watch</router-link>
-      </li>
-    </ul>
-    <form class="form-inline search-form my-2 my-lg-0"
-     @submit.stop.prevent="searchMovies"
-     :class="{ 'search-active' : searchActive }">
-      <input v-model="keyword" @focus="searchFocus" @blur="searchBlur"
-      class="search-input mr-sm-2" type="search" placeholder="請輸入電影名稱" aria-label="Search">
-      <div @click="searchMovies" class="search-icon-wrapper">
-        <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
-      </div>
-    </form>
-  </nav>
-</div>
+  <div class="nav-container">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <router-link class="navbar-brand" to="/">Don’t Forget !</router-link>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/to-watch">To Watch</router-link>
+        </li>
+      </ul>
+      <form
+        class="form-inline search-form my-2 my-lg-0"
+        @submit.stop.prevent="searchMovies"
+        :class="{ 'search-active': searchActive }"
+      >
+        <input
+          v-model="keyword"
+          @focus="searchFocus"
+          @blur="searchBlur"
+          class="search-input mr-sm-2"
+          type="search"
+          placeholder="請輸入電影名稱"
+          aria-label="Search"
+        />
+        <div @click="searchMovies" class="search-icon-wrapper">
+          <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+        </div>
+      </form>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      keyword: '',
-      searchActive: false
-    }
+      keyword: "",
+      searchActive: false,
+    };
   },
   methods: {
     searchFocus() {
-      this.searchActive = true
+      this.searchActive = true;
     },
     searchBlur() {
-      this.searchActive = false
+      this.searchActive = false;
     },
     searchMovies() {
-      if(this.keyword.length === 0) {
-        return
+      if (this.keyword.length === 0) {
+        return;
       }
-      console.log('想搜尋的關鍵字為：'+ this.keyword )
-      document.activeElement.blur()
-    }
-  }
-}
+      console.log("想搜尋的關鍵字為：" + this.keyword);
+      document.activeElement.blur();
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/scss/variable.scss';
-@import url('https://fonts.googleapis.com/css2?family=Splash&display=swap');
+@import "../assets/scss/variable.scss";
+@import url("https://fonts.googleapis.com/css2?family=Splash&display=swap");
 
 .nav-container {
   background-color: $brand-purple;
+  min-width: 460px;
   nav.navbar {
     height: 3.75rem;
     margin: 0 auto;
@@ -61,30 +71,30 @@ export default {
 
 .navbar-brand {
   user-select: none;
-  font-family: 'Splash', cursive;
-  color: #DAA520;
+  font-family: "Splash", cursive;
+  color: #daa520;
   &:focus {
-    color: #DAA520;
+    color: #daa520;
   }
 }
 
 form.search-form {
   font-size: 0.9em;
   &:hover {
-    border-bottom: 1px solid rgba(255,255,255,.5); 
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
   }
   &.search-active {
-    border-bottom: 1px solid rgba(255,255,255,.5); 
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
   }
   input.search-input {
     background-color: transparent;
     border: 0;
     outline: none; /* 外框效果 */
-    color: #FFF;
+    color: #fff;
     margin: 0 6px 0 12px;
-    
-    &::placeholder{
-      color: #FFF;
+
+    &::placeholder {
+      color: #fff;
       opacity: 0.6;
     }
   }
@@ -94,10 +104,8 @@ form.search-form {
   }
 }
 
-
-
 /* media query */
-// Small devices , > 576px 
+// Small devices , > 576px
 @media screen and (min-width: 576px) {
   .navbar-brand {
     font-size: 1.4em;
@@ -119,5 +127,4 @@ form.search-form {
 
 // Extra large devices , > 1200px
 // @media screen and (min-width: 1200px) { ... }
-
 </style>

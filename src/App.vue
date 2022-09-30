0a1beeed8209e446d84c7055bbf55d25f0cd7b74
Jater <p833795p@gmail.com>
2022-09-30 22:59:29 +0800

    feat: 增加Footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="dont-forget-movie">
     <Navbar />
-    <router-view/>
+    <router-view />
+    <Footer />
   </div>
 </template>
 
-
 <script>
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './../src/components/Navbar.vue'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./../src/components/Navbar.vue";
+import Footer from "./../src/components/Footer.vue";
 
 export default {
- name: 'App',
- components: {
-   Navbar
- }
-}
+  name: "App",
+  components: {
+    Navbar,
+    Footer,
+  },
+};
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="footer-container">
+    <footer class="footer">
+      <div class="info">
+        <a
+          href="https://github.com/KUOCHIUHSIANG"
+          target="_blank"
+          class="github"
+        >
+          ©2022 Jater Kuo
+        </a>
+      </div>
+      <div class="powered">
+        Powered by
+        <div class="logo-container">
+          <a
+            href="https://www.themoviedb.org/documentation/api"
+            target="_blank"
+          >
+            <img src="../assets/images/TMDBlogo.svg" alt="" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variable.scss";
+
+a,
+a:hover,
+a:focus {
+  color: $text-main;
+}
+
+.footer-container {
+  background-color: $brand-purple;
+  min-width: 460px;
+  footer.footer {
+    height: 5rem;
+    margin: 0 auto;
+    padding: 0.5rem 1rem;
+    max-width: 1200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .info {
+      margin-right: 2rem;
+      a {
+        color: #ffffff77;
+        &:hover {
+          color: $text-main;
+        }
+      }
+    }
+    .powered {
+      color: #ffffff77;
+      display: flex;
+      align-items: center;
+      .logo-container {
+        width: 100px;
+        margin-left: 0.5rem;
+      }
+    }
+  }
+}
+</style>
